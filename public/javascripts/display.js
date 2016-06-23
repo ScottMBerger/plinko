e8638541af716572c3699b71d1ea6dd836e97bcd
ScottMBerger <contact@scottberger.xyz>
2016-06-22 22:00:04 -0400

    Refactor sockets and components

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,130 +1,151 @@
 
-var socket = io();
 var app = angular.module('app', ['ngAnimate']);
 
-app.controller('betting', function($scope, $interval) {
+app.factory('socket', function ($rootScope) {
+	var socket = io();
+	console.log(socket.io.engine.id);
+	return {
+		id: function (eventName, callback) {
+			$rootScope.socketid = socket.io.engine.id.substr(1, 4);
+		},
+		on: function (eventName, callback) {
+			socket.on(eventName, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function (eventName, data, callback) {
+			socket.emit(eventName, data, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					if (callback) {
+						callback.apply(socket, args);
+					}
+				});
+			})
+		},
+		removeAllListeners: function (eventName, callback) {
+			socket.removeAllListeners(eventName, function() {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(socket, args);
+				});
+			}); 
+		}
+	};
+});
+
+
+function ChatBoxController($scope, socket) {
+	$scope.messages = [];
+
+	socket.on('chat message', function(msg){
+		$scope.messages.push(msg);
+	});
+
+	$scope.addMessage = function(message) {
+		socket.emit('chat message', message);
+		$scope.messages.push({user: "You", msg: message});
+		$scope.newMessage = '';
+	};
+}
+
+app.component('chatbox', {
+	templateUrl: '/views/chatbox.html',
+	controller: ChatBoxController
+});
+
+function RecentListController($scope, socket) {
+	$scope.recent = [];
+
+	socket.on('payoutState', function(msg){
+		$scope.recent.unshift(msg.spot);
+	});
+}
+
+app.component('recentlist', {
+	templateUrl: '/views/recentlist.html',
+	controller: RecentListController
+});
+
+
+app.controller('betting', function($scope, $interval, socket) {
 	$scope.credits = false;
 	$scope.bettingTime = false;
-  $scope.leftBets = {"total":0, "list":[]};
-  $scope.midBets = {"total":0, "list":[]};
-  $scope.rightBets = {"total":0, "list":[]};
-  timerPromise = false;
-  $scope.messages = [];
-  $scope.recent = [];
+	$scope.leftBets = {"total":0, "list":[]};
+	$scope.midBets = {"total":0, "list":[]};
+	$scope.rightBets = {"total":0, "list":[]};
+	var timerPromise = false;
 
-  $scope.getTimeRemaining = function(){
-  	var now = new Date();
-    elapsedMs = now.getTime() - $scope.bett.getTime();
-	  return elapsedMs;
-	}
-
-	$('#lbutton').click(function(){
-		if ($scope.bettingTime && $scope.credits && $scope.credits > 0 && $('#linput').val() > 0) {
-		  socket.emit('left bet', $('#linput').val());
-		  console.log($('#linput').val());
-		  //$('#linput').val('');
-		  return false;
+	$scope.leftBet = function(amount) {
+		if ($scope.bettingTime && $scope.credits && $scope.credits > 0 && amount > 0 && amount <= $scope.credits) {
+			socket.emit('left bet', amount);
 		}
-	});
+	};
 
-	$('#mbutton').click(function(){
-		if ($scope.bettingTime && $scope.credits && $scope.credits > 0 && $('#minput').val() > 0) {
-		  socket.emit('middle bet', $('#minput').val());
-		  //$('#minput').val('');
-		  return false;
+	$scope.midBet = function(amount) {
+		if ($scope.bettingTime && $scope.credits && $scope.credits > 0 && amount > 0 && amount <= $scope.credits) {
+			socket.emit('middle bet', amount);
 		}
-	});
+	};
 
-	$('#rbutton').click(function(){
-		if ($scope.bettingTime && $scope.credits && $scope.credits > 0 && $('#rinput').val() > 0) {
-		  socket.emit('right bet', $('#rinput').val());
-		  //$('#rinput').val('');
-		  return false;
-		 }
-	});
+	$scope.rightBet = function(amount) {
+		if ($scope.bettingTime && $scope.credits && $scope.credits > 0 && amount > 0 && amount <= $scope.credits) {
+			socket.emit('right bet', amount);
+		}
+	};
 
-	socket.on('connect', function(){
-		$scope.$apply(function () {
-	    $scope.socketid = socket.io.engine.id.substr(1, 4);
-	  });
-	});
+	socket.on('connect', function(msg){
+		socket.id();
+	 });
 
-  socket.on('betState', function(msg){
-	  $scope.$apply(function () {
-	  	$scope.payout = '';
-      $scope.leftBets = {"total":0, "list":[]};
-      $scope.midBets = {"total":0, "list":[]};
-  		$scope.rightBets = {"total":0, "list":[]};
-  		
+	socket.on('betState', function(msg){
+		$scope.payout = '';
+		$scope.leftBets = {"total":0, "list":[]};
+		$scope.midBets = {"total":0, "list":[]};
+		$scope.rightBets = {"total":0, "list":[]};
 
-      $scope.bettingTime = new Date();
-      timerPromise = $interval(function() {
-        var now = new Date();
+		$scope.bettingTime = new Date();
+		timerPromise = $interval(function() {
+			var now = new Date();
         //$scope.time = now;
         $scope.elapsedMs = 5000 + $scope.bettingTime.getTime() - now.getTime();
       }, 31);
 
-    });
 	});
 
 	socket.on('payoutState', function(msg){
-	  $scope.$apply(function () {
-	  	$scope.recent.unshift(msg.spot);
-	  	$scope.payout = msg.spot;
-  		
-    });
+		$scope.payout = msg.spot;
 	});
 
 
 	socket.on('startState', function(msg){
-	  $scope.$apply(function () {
-	  	if(timerPromise){
-	  		$interval.cancel(timerPromise);
-	  	};
-	  	
-  		$scope.bettingTime = false;
-    });
+		if(timerPromise){
+			$interval.cancel(timerPromise);
+		};
+
+		$scope.bettingTime = false;
 	});
 
 	socket.on('left message', function(amount, user){
-	  $scope.$apply(function () {
-	  		$scope.leftBets.total += Number(amount);
-        $scope.leftBets.list.push(user.substr(3, 4)+" "+amount);
-    });
+		$scope.leftBets.total += Number(amount);
+		$scope.leftBets.list.push(user.substr(3, 4)+" "+amount);
 	});
 
 	socket.on('middle message', function(amount, user){
-	  $scope.$apply(function () {
-	  		$scope.midBets.total += Number(amount);
-        $scope.midBets.list.push(user.substr(3, 4)+" "+amount);
-    });
+		$scope.midBets.total += Number(amount);
+		$scope.midBets.list.push(user.substr(3, 4)+" "+amount);
 	});
 
 	socket.on('right message', function(amount, user){
-	  $scope.$apply(function () {
-	  		$scope.rightBets.total += Number(amount);
-        $scope.rightBets.list.push(user.substr(3, 4)+" "+amount);
-    });
+		$scope.rightBets.total += Number(amount);
+		$scope.rightBets.list.push(user.substr(3, 4)+" "+amount);
 	});
 
 	socket.on('updateCredits', function (credits) {
-      $scope.credits = credits;
-  });
-
-	//chat
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $scope.$apply(function () {
-    	$scope.messages.push("You: "+$('#m').val());
-    });
-    $('#m').val('');
-    return false;
-  });
-
-  socket.on('chat message', function(msg){
-  	$scope.$apply(function () {
-    	$scope.messages.push(msg);
-    });
-  });
+		$scope.credits = credits;
+	});
 });
 
